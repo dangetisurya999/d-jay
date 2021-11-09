f62ae0649a5d5f3229025804815271b24571b8f2
dangetisurya999 <93986515+dangetisurya999@users.noreply.github.com>
2021-11-09 17:47:37 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	int a=10,b=20;
+	 int t=a;
+	a=b;
+	b=t;
+	System.out.println("A IS "+a+"  "+"B IS "+b);
+	}
+}
